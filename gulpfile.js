43ab5a56fd0cf7896d56895968e8300d40480526
vanzel <Vanzel5@yandex.ru>
2023-12-18 16:47:55 +0300

    reduce footer

--- gulpfile.js
+++ gulpfile.js
@@ -87,9 +87,9 @@ function browsersync() {
 		server: { baseDir: baseDir + '/' },
 		notify: false,
 		online: online,
-		index: '/templates/developmentPage.html',
+		// index: '/templates/developmentPage.html',
 		// index: '/templates/insidePage.html',
-		// index: '/templates/tablePage.html',
+		index: '/templates/tablePage.html',
 		// host: '192.168.50.236'
 	})
 }
